f1148abb6eec13607920b66713c1b82d8cbb35a6
mohitpaudyal <mohit.paudyal@gmail.com>
2020-05-26 16:26:15 -0400

    updating the css for the menu header

--- insulin-tracker/src/components/layouts/navbar.js
+++ insulin-tracker/src/components/layouts/navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import SignedInLinks from './signedInLinks';
 import SignedOutLinks from './signOutLinks';
 import {connect} from "react-redux";
-import {getStateUsername, getUsername} from "../sign-in/sign-in.reducer";
+import {getStateUsername} from "../sign-in/sign-in.reducer";
 
 
 const NavBar = (props) => {
@@ -13,7 +13,7 @@ const NavBar = (props) => {
         <nav className="nav-wrapper teal darken-3">
             <div className="container">
                 <Link to="/" className="brand-logo left">
-                    Project Insulin Bond
+                    Insulin Tracker
                 </Link>
                 {props.username$ ? <SignedInLinks/> : <SignedOutLinks/>}
             </div>
